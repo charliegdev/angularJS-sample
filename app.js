d80f170d65ff788d59dbee673ab2e59fddca336e
Charlie Guan <charliegdev@gmail.com>
2018-02-24 19:43:58 -0800

    Use ng-repeat-start and ng-repeat-end to repeat something over multiple HTML element that are not wrappable inside one div.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 angular.module("notesApp", []).controller("MainCtrl", [function () {
-    let notes = [
+    this.notes = [
         {
             id: 1,
             label: "First Note",
@@ -17,31 +17,4 @@ angular.module("notesApp", []).controller("MainCtrl", [function () {
             done: true
         }
     ];
-
-    this.notes1 = angular.copy(notes);
-    this.notes2 = angular.copy(notes);
-
-    this.changeNotes = () => {
-        notes = [
-            {
-                id: 1,
-                label: "Changed Note",
-                done: false,
-                someRandom: 4242
-            },
-            {
-                id: 2,
-                label: "Second Note",
-                done: false,
-            },
-            {
-                id: 3,
-                label: "Finished Third Note",
-                done: true,
-            },
-        ];
-
-        this.notes1 = angular.copy(notes);
-        this.notes2 = angular.copy(notes);
-    };
 }]);
