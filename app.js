a282ee04c06078886197d6905737603c997b9ca7
Charlie Guan <charliegdev@gmail.com>
2018-02-24 18:04:03 -0800

    Use ng-repeat on an object.

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 angular.module("notesApp", []).controller("MainCtrl", [function () {
-    this.notes = [
-        { id: 1, label: "First Note", done: false },
-        { id: 2, label: "Second Note", done: false },
-        { id: 3, label: "Done Note", done: true },
-        { id: 4, label: "Last Note", done: false }
-    ];
+    this.notes = {
+        shyam: { id: 1, label: "First Note", done: false },
+        Misko: { id: 3, label: "Finished Third Note", done: true },
+        brad: { id: 2, label: "Second Note", done: false },
+    };
 }]);
