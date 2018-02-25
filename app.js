bd1e7fa9f73d9435efb277556e071b320220da58
Charlie Guan <charliegdev@gmail.com>
2018-02-24 19:35:26 -0800

    Use note.id to track ng-repeat, instead of using default hash.

--- app.js
+++ app.js
@@ -1,7 +1,47 @@
 angular.module("notesApp", []).controller("MainCtrl", [function () {
-    this.notes = {
-        shyam: { id: 1, label: "First Note", done: false },
-        Misko: { id: 3, label: "Finished Third Note", done: true },
-        brad: { id: 2, label: "Second Note", done: false },
+    let notes = [
+        {
+            id: 1,
+            label: "First Note",
+            done: false,
+            someRandom: 31431
+        },
+        {
+            id: 2,
+            label: "Second Note",
+            done: false
+        },
+        {
+            id: 3,
+            label: "Finished Third Note",
+            done: true
+        }
+    ];
+
+    this.notes1 = angular.copy(notes);
+    this.notes2 = angular.copy(notes);
+
+    this.changeNotes = () => {
+        notes = [
+            {
+                id: 1,
+                label: "Changed Note",
+                done: false,
+                someRandom: 4242
+            },
+            {
+                id: 2,
+                label: "Second Note",
+                done: false,
+            },
+            {
+                id: 3,
+                label: "Finished Third Note",
+                done: true,
+            },
+        ];
+
+        this.notes1 = angular.copy(notes);
+        this.notes2 = angular.copy(notes);
     };
 }]);
