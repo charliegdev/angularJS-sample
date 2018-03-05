90ff0b1e515f02c753533af1c29430a291df0aaa
Charlie Guan <charliegdev@gmail.com>
2018-03-04 16:19:39 -0800

    Simplest example of using a $log service.

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,7 @@
 angular.module("notesApp", []);
 
-angular.module("notesApp").controller("MainCtrl", [function () {
-    this.tab = "first";
-    this.open = (tab) => {
-        this.tab = tab;
-    };
-}]);
-
-angular.module("notesApp").controller("SubCtrl", [function () {
-    this.list = [
-        { id: 1, label: "Item 0" },
-        { id: 2, label: "Item 1" }
-    ];
-
-    this.add = () => {
-        this.list.push({
-            id: this.list.length + 1,
-            label: "Item " + this.list.length
-        });
+angular.module("notesApp").controller("MainCtrl", ["$log", function ($log) {
+    this.logStuff = () => {
+        $log.log("Some stuff");
     };
 }]);
