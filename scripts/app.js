733eafccb226ed4ab31fae594bbfca696455f36f
Charlie Guan <charliegdev@gmail.com>
2018-03-04 18:09:53 -0800

    Add a service to store items. There is only 1 list so 2 tabs share the same item list.

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,34 @@
 angular.module("notesApp", []);
 
-angular.module("notesApp").controller("MainCtrl", ["$log", function ($log) {
-    this.logStuff = () => {
-        $log.log("Some stuff");
+angular.module("notesApp").controller("MainCtrl", [function () {
+    this.tab = "first";
+    this.open = newTab => {
+        this.tab = newTab;
+    };
+}]);
+
+angular.module("notesApp").controller("SubCtrl", ["ItemService", function (ItemService) {
+    this.list = () => ItemService.list();
+    this.add = () => {
+        ItemService.add({
+            id: this.list().length + 1,
+            label: "Item " + this.list().length
+        });
+    };
+}]);
+
+angular.module("notesApp").factory("ItemService", [function () {
+    const items = [
+        { id: 1, label: "Item 0" },
+        { id: 2, label: "Item 1" }
+    ];
+
+    return {
+        list: function () {
+            return items;
+        },
+        add: function (item) {
+            items.push(item);
+        }
     };
 }]);
