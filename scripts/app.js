753d2334ab2f252d82f6185c0d7ee778738d074f
Charlie Guan <charliegdev@gmail.com>
2018-03-04 19:58:43 -0800

    Add server file. Can't serve index.html for now.

--- scripts/app.js
+++ scripts/app.js
@@ -1,34 +1,10 @@
 angular.module("notesApp", []);
 
-angular.module("notesApp").controller("MainCtrl", [function () {
-    this.tab = "first";
-    this.open = newTab => {
-        this.tab = newTab;
-    };
-}]);
-
-angular.module("notesApp").controller("SubCtrl", ["ItemService", function (ItemService) {
-    this.list = () => ItemService.list();
-    this.add = () => {
-        ItemService.add({
-            id: this.list().length + 1,
-            label: "Item " + this.list().length
-        });
-    };
-}]);
-
-angular.module("notesApp").factory("ItemService", [function () {
-    const items = [
-        { id: 1, label: "Item 0" },
-        { id: 2, label: "Item 1" }
-    ];
-
-    return {
-        list: function () {
-            return items;
-        },
-        add: function (item) {
-            items.push(item);
-        }
-    };
+angular.module("notesApp").controller("MainCtrl", ["$http", "$log", function ($http, $log) {
+    this.items = [];
+    $http.get("/api/note").then(response => {
+        this.items = response.data;
+    }, err => {
+        $log.debug(err);
+    });
 }]);
