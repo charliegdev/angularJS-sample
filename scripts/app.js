d7ca507e76bb2b221ebd1d21bec22ae72accf205
Charlie Guan <charliegdev@gmail.com>
2018-03-04 16:16:14 -0800

    Add different controllers to see how controllers are not suitable to store persistant data.

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,22 @@
-angular.module("notesApp", []).controller("MainCtrl", [function () {
-    this.user = {
-        username: "",
-        password: ""
-    };
+angular.module("notesApp", []);
 
-    this.reset = () => {
-        this.user = {
-            username: "",
-            password: ""
-        };
+angular.module("notesApp").controller("MainCtrl", [function () {
+    this.tab = "first";
+    this.open = (tab) => {
+        this.tab = tab;
     };
+}]);
 
-    this.submit = () => {
-        console.log(this.user);
+angular.module("notesApp").controller("SubCtrl", [function () {
+    this.list = [
+        { id: 1, label: "Item 0" },
+        { id: 2, label: "Item 1" }
+    ];
+
+    this.add = () => {
+        this.list.push({
+            id: this.list.length + 1,
+            label: "Item " + this.list.length
+        });
     };
 }]);
