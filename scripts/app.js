a8226d82dcf6300ef949543b2fdd9803416666e3
Charlie Guan <cguan@blastworksinc.com>
2018-03-06 11:26:32 -0800

    Chain promises to add new todos.

--- scripts/app.js
+++ scripts/app.js
@@ -2,9 +2,28 @@ angular.module("notesApp", []);
 
 angular.module("notesApp").controller("MainCtrl", ["$http", "$log", function ($http, $log) {
     this.items = [];
+    this.newTodo = {};
+
+    const fetchTodos = () => {
+        return $http.get("/api/note").then(response => {
+            this.items = response.data;
+        }, error => {
+            $log.debug(error);
+        });
+    };
+
+    fetchTodos();
+    
+    this.add = () => {
+        $http.post("/api/note", this.newTodo).then(fetchTodos).then(response => {
+            this.newTodo = {};
+        });
+    };
+
     $http.get("/api/note").then(response => {
         this.items = response.data;
     }, err => {
         $log.debug(err);
     });
+
 }]);
