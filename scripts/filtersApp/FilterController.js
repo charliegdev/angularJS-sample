6ee0a1de6ea3916d36091250078c62113bd9bc8c
Charlie Guan <charliegdev@gmail.com>
2018-03-22 22:59:56 -0700

    Add our own timeAgo filter.

--- scripts/filtersApp/FilterController.js
+++ scripts/filtersApp/FilterController.js
@@ -1,23 +1,4 @@
 angular.module("filtersApp").controller("FilterController", [function () {
-    this.notes = [
-        { label: 'FC Todo', type: 'chore', done: false },
-        { label: 'FT Todo', type: 'task', done: false },
-        { label: 'FF Todo', type: 'fun', done: true },
-        { label: 'SC Todo', type: 'chore', done: false },
-        { label: 'ST Todo', type: 'task', done: true },
-        { label: 'SF Todo', type: 'fun', done: true },
-        { label: 'TC Todo', type: 'chore', done: false },
-        { label: 'TT Todo', type: 'task', done: false },
-        { label: 'TF Todo', type: 'fun', done: false }
-    ];
-
-    this.sortOrder = ["+type", "-label"];
-
-    this.filterOptions = {
-        "string": "",
-        "object": { done: false, label: "C" },
-        "function": note => note.type === "task" && note.done === false
-    };
-
-    this.currentFilter = "string";
+    this.startTime = new Date().getTime();
+    this.someTimeAgo = new Date().getTime() - (1000 * 60 * 60 * 4);
 }]);
