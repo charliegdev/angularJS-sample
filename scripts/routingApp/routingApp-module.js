c0ec6544ced8df623e373256b878c34091a61fa6
Charlie Guan <charliegdev@gmail.com>
2018-03-24 19:46:08 -0700

    A very simple example of routing.

--- scripts/routingApp/routingApp-module.js
+++ scripts/routingApp/routingApp-module.js
@@ -1,1 +1,10 @@
-angular.module("routingApp", ["ngRoute"]);
+angular.module("routingApp", ["ngRoute"]).config(["$routeProvider", function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            template: "<h5>This is the default route.</h5>"
+        })
+        .when("/second", {
+            template: "<h5>This is the second route.</h5>"
+        })
+        .otherwise({ redirectTo: "/"});
+}]);
