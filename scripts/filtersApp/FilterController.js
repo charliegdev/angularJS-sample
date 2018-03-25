fa3ec0ed444766be8aeeeab5bd47d7796bb89409
Charlie Guan <charliegdev@gmail.com>
2018-03-24 17:18:54 -0700

    "Add simple unit tests for timeAgo filter."

--- scripts/filtersApp/FilterController.js
+++
@@ -1,4 +0,0 @@
-angular.module("filtersApp").controller("FilterController", [function () {
-    this.startTime = new Date().getTime();
-    this.someTimeAgo = new Date().getTime() - (1000 * 60 * 60 * 4);
-}]);
