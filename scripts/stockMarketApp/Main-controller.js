aaadd517e8636c381a0ae34d69ee3a6f6bb6e183
Charlie Guan <charliegdev@gmail.com>
2018-03-31 16:20:04 -0700

    Add simple ng-include for a new stockMarketApp, to show that we don't need directives just for template reuse.

---
+++ scripts/stockMarketApp/Main-controller.js
@@ -0,0 +1,12 @@
+angular.module("stockMarketApp").controller("MainController", [function () {
+    this.stocks = [
+        { name: "1st Stock", price: 100, previous: 220 },
+        { name: "2nd Stock", price: 140, previous: 120 },
+        { name: "3rd Stock", price: 110, previous: 110 },
+        { name: "4th Stock", price: 400, previous: 420 }
+    ];
+
+    this.stockTemplateUrl = "scripts/stockMarketApp/stock.html";
+
+    this.getChangePercent = stock => Math.ceil((stock.price - stock.previous) / stock.previous * 100) + "%";
+}]);
