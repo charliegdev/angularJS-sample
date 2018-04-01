e366f1e4d056347eca6b4c202b567fbd38cb2a30
Charlie Guan <charliegdev@gmail.com>
2018-04-01 12:22:18 -0700

    Move getChangePercent function from controller to directive.

--- scripts/stockMarketApp/Main-controller.js
+++ scripts/stockMarketApp/Main-controller.js
@@ -6,8 +6,5 @@ angular.module("stockMarketApp").controller("MainController", [function () {
         { name: "4th Stock", price: 400, previous: 420 }
     ];
 
-
-    this.getChangePercent = stock => Math.ceil((stock.price - stock.previous) / stock.previous * 100) + "%";
-
     this.currentTab = "tab1";
 }]);
--- scripts/stockMarketApp/stockWidget-directive.js
+++ scripts/stockMarketApp/stockWidget-directive.js
@@ -1,5 +1,9 @@
 angular.module("stockMarketApp").directive("stockWidget", [function () {
+    function link(scope, element, attr) {
+        scope.getChangePercent = stock => Math.ceil((stock.price - stock.previous) / stock.previous * 100) + "%";
+    }
     return {
-        templateUrl: "scripts/stockMarketApp/stock.html"
+        templateUrl: "scripts/stockMarketApp/stock.html",
+        link
     };
 }]);
