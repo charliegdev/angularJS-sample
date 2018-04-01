17dbb86efcf673433831d3d3ee701bdbe1aff8ee
Charlie Guan <charliegdev@gmail.com>
2018-04-01 16:43:07 -0700

    Remove experimental stuff.

--- scripts/stockMarketApp/stockWidget-directive.js
+++ scripts/stockMarketApp/stockWidget-directive.js
@@ -1,7 +1,6 @@
 angular.module("stockMarketApp").directive("stockWidget", [function () {
     function link(scope, element, attr) {
         scope.getChangePercent = stock => Math.ceil((stock.price - stock.previous) / stock.previous * 100) + "%";
-        scope.changeStock = () => { scope.stockData = { name: "Directive Stock", price: 500, previous: 200 }; };
     }
     return {
         templateUrl: "scripts/stockMarketApp/stock.html",
